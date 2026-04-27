d51110d6cf19b1eb7171e4b22d8f17d8f2c9e4ab
Ryuichi Lin <ryuno107lin@gmail.com>
2026-04-27 11:09:56 -0400

    feat: initialize all devices in mouse initialization function

--- initialization/initialization.c
+++ initialization/initialization.c
@@ -8,8 +8,16 @@
 /*----------------------------------------------------------------------------*/
 /*                               Include Files                                */
 /*----------------------------------------------------------------------------*/
+#include <stdbool.h>
 #include <stdint.h>
 #include "processor.h"
+#include "battery_comparator.h"
+#include "led.h"
+#include "power_enabler.h"
+#include "pushbutton.h"
+#include "infrared_sensor.h"
+#include "magnetic_encoder.h"
+#include "vacuum.h"
 
 /*----------------------------------------------------------------------------*/
 /*                         Private Function Prototypes                        */
@@ -27,10 +35,24 @@
 void init_mouse(void)
 {
     init_processor();
+    init_battery_comparator();
+    init_leds();
+    init_power_enabler();
+    init_pushbutton();
+    init_infrared_sensors();
+    init_magnetic_encoders();
+    init_vacuum();
 }
 
 void deinit_mouse(void)
 {
+    deinit_vacuum();
+    deinit_magnetic_encoders();
+    deinit_infrared_sensors();
+    deinit_pushbutton();
+    deinit_power_enabler();
+    deinit_leds();
+    deinit_battery_comparator();
     deinit_processor();
 }
 
